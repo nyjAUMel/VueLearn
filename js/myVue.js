2100d5eb3b467f1ddfb6b7dc48969505d5ee8f83
夕川AU <917764460@qq.com>
2025-06-06 18:22:27 +0800

    21Vue源码解读 - 30计算属性的简写形式

--- js/myVue.js
+++ js/myVue.js
@@ -1,17 +1,25 @@
 class Vue {
-    // options是一个对象
-    constructor(options) { 
-        // 获取所有属性名
-        Object.key(options.data).forEach(key => {
-            Object.defineProperty(this, key, {
-                get() {
-                    // 返回该对象的属性data的值
-                    return options.data[propertyName]
-                },
-                set(newValue) {
-                    options.data[propertyName] = newValue
-                }
-            })
-        })
-    }
+  // options是一个对象
+  constructor(options) {
+    // 获取所有属性名
+    /* Object.keys(options.data).forEach((key) => {
+      let firstChar = key.charAt(0);
+      // 如果属性名的第一个字符是$或_，则不代理
+      if (firstChar !== "$" && firstChar !== "_") {
+        Object.defineProperty(this, key, {
+          get() {
+            // 返回该对象的属性data的值
+            return options.data[key];
+          },
+          set(newValue) {
+            options.data[key] = newValue;
+          },
+        });
+      }
+    }); */
+
+    Object.keys(options.methods).forEach((key) => {
+      this[key] = options.methods[key];
+    });
+  }
 }
